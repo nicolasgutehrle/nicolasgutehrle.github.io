e35072a8c9f7b68ac1ca32ddd2bbc85890db3d70
Nicolas Gutehrlé <nicolasgutehrle@nicolas.local>
2021-11-30 10:11:48 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+
+ 
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+
+           
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.fontSize = "2vw";
+	tablinks[i].style.borderBottom = "none";
+
+  }
+
+    document.getElementById(pageName).style.display="block";
+   
+    elmnt.style.borderBottom = "thick solid #ff017d";
+    elmnt.style.fontSize = "3vw";
+
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
